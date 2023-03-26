0893079257697119539cd62fc0a1f152e0f1d910
harisljubovic <123327911+harisljubovic@users.noreply.github.com>
2023-03-27 00:19:58 +0200

    Add files via upload

---
+++ CylinderVolume/script.js
@@ -0,0 +1,7 @@
+var radius = prompt("Enter radius value (r):");
+var height = prompt("Enter height value (h):");
+const pi = 3.1415;
+
+var volume = pi * parseInt(radius) * parseInt(radius) * parseInt(height);
+
+alert("Volume of cylinder is: " + volume);
